4f3c629b8882d8b8619511b1bc64d2ae10c6673f
Nyuu <126999070+lucynyuu@users.noreply.github.com>
2023-12-09 03:05:51 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,8 +1,6 @@
 %include "play_song.inc"
 extern _MessageBoxA@16
 extern _ExitProcess@4
-extern _GetConsoleWindow@0
-extern _ShowWindow@8
 extern _printf
 ;extern _scanf
 
@@ -16,11 +14,6 @@ section .text
      global _main
 
 _main:
-    call _GetConsoleWindow@0
-    push 0
-    push eax
-    call _ShowWindow@8
-
     push 4
     push title
     push message
